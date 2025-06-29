d71451005d2f058f4ae13bfe43a836e056fea521
Boston343 <branden.austin1@gmail.com>
2025-06-29 00:10:25 -0400

    working on radio group

---
+++ apps/demo/src/components/starwind/radio-group/index.ts
@@ -0,0 +1,9 @@
+import RadioGroup from "./RadioGroup.astro";
+import RadioGroupItem from "./RadioGroupItem.astro";
+
+export { RadioGroup, RadioGroupItem };
+
+export default {
+	Root: RadioGroup,
+	Item: RadioGroupItem,
+};
