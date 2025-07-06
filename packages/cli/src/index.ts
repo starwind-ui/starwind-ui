8fa6efbd313625da29c4841a4e029c16876bdfa5
boston343 <reap3r@mailfence.com>
2025-07-06 11:58:12 -0400

    version bumps

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -9,7 +9,7 @@ import { update } from "./commands/update.js";
 const program = new Command()
   .name("starwind")
   .description("Add beautifully designed components to your Astro applications")
-  .version("1.7.0");
+  .version("1.7.1");
 
 program
   .command("init")
