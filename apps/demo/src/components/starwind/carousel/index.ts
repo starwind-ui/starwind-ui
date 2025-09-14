2c3cd27e3752d3a8d61cc20969cbb3259949bef7
Boston343 <reap3r@mailfence.com>
2025-09-14 15:24:21 -0400

    carousel WIP

---
+++ apps/demo/src/components/starwind/carousel/index.ts
@@ -0,0 +1,15 @@
+import Carousel from "./Carousel.astro";
+import CarouselContent from "./CarouselContent.astro";
+import CarouselItem from "./CarouselItem.astro";
+import CarouselNext from "./CarouselNext.astro";
+import CarouselPrevious from "./CarouselPrevious.astro";
+
+export { 
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+};
+
+export default Carousel;
