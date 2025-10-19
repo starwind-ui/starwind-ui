d6d95414fc362e8d933194a4473f54f30925df03
n4n1t0 <adrian.alvarezalonso1991@gmail.com>
2025-10-19 18:21:29 +0200

    refactor(kbd): export tv variants and update exports structure
    Export kbd and kbdGroup tv variants from their respective components and restructure exports in index.ts to include KbdVariants object for better maintainability and usage flexibility

--- packages/core/src/components/kbd/index.ts
+++ packages/core/src/components/kbd/index.ts
@@ -1,7 +1,9 @@
-import Kbd from "./Kbd.astro";
-import KbdGroup from "./KbdGroup.astro";
+import Kbd, { kbd } from "./Kbd.astro";
+import KbdGroup, { kbdGroup } from "./KbdGroup.astro";
 
-export { Kbd, KbdGroup };
+const KbdVariants = { kbd, kbdGroup };
+
+export { Kbd, KbdGroup, KbdVariants };
 
 export default {
   Root: Kbd,
