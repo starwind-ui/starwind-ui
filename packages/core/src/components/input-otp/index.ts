7c88738f6ee3ec4cec0cb8bda0bd7a7aa5b17983
Boston343 <branden.austin1@gmail.com>
2026-01-04 15:14:15 +0700

    feat(core/input-otp): update component with new items from demo project

--- packages/core/src/components/input-otp/index.ts
+++ packages/core/src/components/input-otp/index.ts
@@ -4,6 +4,9 @@ import InputOtpSeparator, { inputOtpSeparator } from "./InputOtpSeparator.astro"
 import InputOtpSlot, { inputOtpSlot } from "./InputOtpSlot.astro";
 import type { InputOtpChangeEvent } from "./InputOtpTypes";
 
+const REGEXP_ONLY_DIGITS_AND_CHARS = /^[A-Za-z0-9]+$/;
+const REGEXP_ONLY_DIGITS = /^[0-9]+$/;
+
 const InputOtpVariants = {
   inputOtp,
   inputOtpGroup,
@@ -18,6 +21,8 @@ export {
   InputOtpSeparator,
   InputOtpSlot,
   InputOtpVariants,
+  REGEXP_ONLY_DIGITS,
+  REGEXP_ONLY_DIGITS_AND_CHARS,
 };
 
 export default {
@@ -26,4 +31,3 @@ export default {
   Separator: InputOtpSeparator,
   Slot: InputOtpSlot,
 };
-
